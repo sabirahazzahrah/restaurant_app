d999bc3b9dbbd12aa10cd396bee4d45f046cf567
sabirahazzahrah <sabirahazzahrah@gmail.com>
2023-11-13 01:40:53 +0700

    deploy

--- CMS/src/routers/index.jsx
+++ CMS/src/routers/index.jsx
@@ -13,13 +13,13 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
-    loader: () => {
-      const token = localStorage.getItem("access_token");
-      if (token) {
-        return redirect("/cuisines");
-      }
-      return null;
-    },
+    // loader: () => {
+    //   const token = localStorage.getItem("access_token");
+    //   if (token) {
+    //     return redirect("/cuisines");
+    //   }
+    //   return null;
+    // },
   },
   {
     loader: () => {
